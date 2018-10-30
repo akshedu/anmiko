e226d49ca9d6deac9d3b639773308ee8d5a916fb
Akshansh Gupta <akshedu@gmail.com>
2018-10-30 12:50:26 +0530

    Add user serialiser and tests

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,4 +1,10 @@
 from django.urls import path, include
+from src.core import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('users', views.UserViewSet)
 
 urlpatterns = [
+    path('', include(router.urls),)
 ]
--- src/core/views.py
+++ src/core/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from .models import User
+from rest_framework import viewsets
+from src.core.serializers import UserSerializer
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
