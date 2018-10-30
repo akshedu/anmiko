26c3684fd12a522fd486cbb4b37596b308cb8769
Akshansh Gupta <akshedu@gmail.com>
2018-10-30 18:37:30 +0530

    custom authentication

--- src/core/models.py
+++ src/core/models.py
@@ -42,6 +42,8 @@ class User(AbstractUser):
 
     username = None
     email = models.EmailField(_('email address'), unique=True)
+    first_name = models.CharField(_('first name'), max_length=150, blank=False)
+    last_name = models.CharField(_('last name'), max_length=150, blank=False)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
--- src/core/serializers.py
+++ src/core/serializers.py
@@ -12,9 +12,15 @@ class UserSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         user = User.objects.create_user(validated_data['email'],
-             validated_data['password'])
+             validated_data['password'],
+             first_name=validated_data['first_name'],
+             last_name=validated_data['last_name'])
+
+        user.set_password(validated_data['password'])
+        user.save()
+
         return user
 
     class Meta:
         model = User
-        fields = ('id', 'email', 'password')
+        fields = ('id', 'first_name', 'last_name', 'email', 'password')
